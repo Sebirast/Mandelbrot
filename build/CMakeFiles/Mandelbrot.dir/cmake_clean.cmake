ed637cf9217b6a5df945701b173378470f5d2362
Sebastian Rast <sebastian.rast@outlook.com>
2022-02-08 21:35:06 +0100

    added cmake and imagecreator

---
+++ build/CMakeFiles/Mandelbrot.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Mandelbrot.dir/Main.cpp.o"
+  "CMakeFiles/Mandelbrot.dir/Main.cpp.o.d"
+  "Mandelbrot"
+  "Mandelbrot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Mandelbrot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
